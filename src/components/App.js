7506cc18a3ce3d1d08f4cf16ccd1147a1ab4e861
18subrato <subratoverma7@gmail.com>
2025-04-14 17:33:00 +0530

    initial

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,15 @@
 
 import React from "react";
+import { useState } from "react";
 import './../styles/App.css';
-import Parent from "./Parent";
+import Child from "./Child";
 
 const App = () => {
+  const [todos,setTodos] = useState(['Learn React','Build a React app','Deploy the React app'])
   return (
     <div>
-       <Parent/>
+       <h1>Parent Component</h1>
+       <Child todos = {todos} setTodos={setTodos}/>
     </div>
   )
 }
--- src/components/Child.jsx
+++ src/components/Child.jsx
@@ -2,11 +2,18 @@ import React, { useState } from 'react'
 
 const Child = ({ todos,setTodos}) => {
 
+    function handleClick(idx){
+        let arr = todos.filter((item,index)=>{
+          return  item.index !== idx;
+        })
+        setTodos(arr);
+        console.log(todos)
+    }
   return (
     <div>
       <h2>Child Component</h2>
       {todos.map((item, idx) => (
-        <li key={idx}>{item}<span><button onClick={(e) => { e.target.remove() }}>Complete</button></span></li>
+        <li key={idx}>{item}<span><button onClick={()=>handleClick(idx)}>Complete</button></span></li>
       ))}
     </div>
   )
